d1f7dc1de01c31b69bf352f5c5b85bddc5a42086
Jameson <90803725+jamesonarnett@users.noreply.github.com>
2022-03-10 21:26:00 -0600

    isPalindrome?

--- srcSection5/WhileDoWhile.java
+++ srcSection5/WhileDoWhile.java
@@ -1,6 +1,6 @@
 public class WhileDoWhile {
     public static void main(String[] args) {
-        int count = 1;
+//        int count = 1;
 
 //        while(count != 6) {
 //            System.out.println("Count value is " + count);
@@ -26,28 +26,53 @@ public class WhileDoWhile {
 //        } while(count != 6);
 
 
-        int number  = 4;
-        int finishedNumber = 20;
-        int evenNumbersFound = 0;
+//        int number  = 4;
+//        int finishedNumber = 20;
+//        int evenNumbersFound = 0;
+//
+//        while(number <= finishedNumber) {
+//            number++;
+//            if(!isEvenNumber(number)) {
+//                continue;
+//            }
+//            System.out.println("Even number " + number);
+//
+//            evenNumbersFound++;
+//            if(evenNumbersFound >= 5) {
+//                System.out.println("We found " + evenNumbersFound + " even numbers");
+//                break;
+//            }
+//
+//
+//        }
+//
+//        System.out.println(sumDigits(1254));
+//
+        System.out.println(isPalindrome(-123321));
 
-        while(number <= finishedNumber) {
-            number++;
-            if(!isEvenNumber(number)) {
-                continue;
-            }
-            System.out.println("Even number " + number);
+    }
 
-            evenNumbersFound++;
-            if(evenNumbersFound >= 5) {
-                System.out.println("We found " + evenNumbersFound + " even numbers");
-                break;
-            }
 
+    public static boolean isPalindrome(int number) {
+        int orginalNumber = number;
+        int reverse = 0;
+
+        while(number != 0) {
+            int remainder = number % 10;
+            System.out.println("Remainder: " + remainder);
+            reverse = (reverse * 10) + remainder;
+            System.out.println("Reverse: " + reverse);
+            number /= 10;
 
         }
 
-
-
+        if(reverse == orginalNumber) {
+            return true;
+        } else {
+            System.out.println("I am num " + number);
+            System.out.println("I am reverse " + reverse);
+            return false;
+        }
 
     }
 
@@ -56,6 +81,22 @@ public class WhileDoWhile {
             return true;
         }
         return false;
+    }
+
+
+    public static int sumDigits(int number) {
+        if(number < 10) {
+            return -1;
+        }
+
+        int sum = 0;
+        while(number > 0) {
+            int digit = number % 10;
+            sum += digit;
+
+            number /= 10;
+        }
+       return sum;
     }
 
 }
